3ae9739f38e7719dd8e5904e23b5a9e0c2b1aaa2
a-r-o-l <alonsoaroldev@gmail.com>
2025-04-19 02:39:12 -0300

    test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,6 @@ function page() {
         <RootHeader />
         <MiniRootHeader />
         <div className="py-20" id="quienes-somos">
-          a
           <Section1 />
           <Section1Mini />
           <Section2 />
