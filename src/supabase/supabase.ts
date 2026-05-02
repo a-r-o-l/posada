997191151ee7eb8a118a7d50bc991a12454dde92
a-r-o-l <alonsoaroldev@gmail.com>
2026-05-02 14:17:09 -0300

    try fix the session 13

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -6,27 +6,7 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabaseServiceRoleKey =
   process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!;
 
-export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    flowType: "implicit",
-    storage: {
-      getItem: (key: string) => {
-        if (typeof window === "undefined") return null;
-        return localStorage.getItem(key);
-      },
-      setItem: (key: string, value: string) => {
-        if (typeof window === "undefined") return;
-        localStorage.setItem(key, value);
-      },
-      removeItem: (key: string) => {
-        if (typeof window === "undefined") return;
-        localStorage.removeItem(key);
-      },
-    },
-  },
-});
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey, {
   auth: {
